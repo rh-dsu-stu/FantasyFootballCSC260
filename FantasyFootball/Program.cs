d7894193c7a30845e01758263abf9bc309cae875
Ryan Halstead <Ryan.Halstead@trojans.dsu.edu>
2019-04-22 13:35:05 -0400

    adding user class and packages.config

--- FantasyFootball/Program.cs
+++ FantasyFootball/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using FantasyData.Api.Client;
 
 namespace FantasyFootball
 {
@@ -10,6 +13,30 @@ namespace FantasyFootball
     {
         static void Main(string[] args)
         {
+            // Connect to client and get data
+            var client = new NFLv3ProjectionsClient("1996605cd1e84deeae0aab46b07dcf83");
+            var projections = client.GetFantasyDefenseProjectionsByGame("2018",7).OrderByDescending(p => p.PointsAllowed).Take(20).ToList();
+
+            // Write data to console
+            foreach (var projection in projections)
+            {
+                Console.WriteLine($"{projection.PlayerID} - {projection.Team} ({projection.PointsAllowed}) Points Allowed by Defense ST: {projection.PointsAllowedByDefenseSpecialTeams}");
+            }
+            Console.ReadKey();
+
+            MongoClient dbClient = new MongoClient("mongodb://127.0.0.1:27017");
+
+            //Database List  
+            var dbList = dbClient.ListDatabases().ToList();
+
+            Console.WriteLine("The list of databases are :");
+            foreach (var item in dbList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+
+
         }
     }
 }
---
+++ FantasyFootball/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace FantasyFootball
+{
+    class User
+    {
+        private string _userName;
+
+        public string UserName => _userName;
+
+
+        public User(string userName)
+        {
+            if (userName == null)
+            {
+                throw new ArgumentNullException(nameof(userName));
+            }
+            _userName = userName;
+        }
+
+        public bool UserNameExists(IMongoDatabase database, string userName)
+        {
+            var filter = new BsonDocument("name", userName);
+            var options = new ListCollectionNamesOptions { Filter = filter };
+
+            return database.ListCollectionNames(options).Any();
+        }
+
+        
+    }
+}
